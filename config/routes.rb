7ea6406b4e0615b9ecb7d98720bbace3230839f6
harunaka224 <114799587+harunaka224@users.noreply.github.com>
2022-12-02 05:20:32 +0000

    「Add]Home/Aboutページ

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  
   devise_for :customers, skip: [:passwords], controllers: {
   registrations: "public/registrations",
   sessions: 'public/sessions'
@@ -6,5 +7,9 @@ Rails.application.routes.draw do
   devise_for :admins, skip: [:registrations, :passwords], controllers: {
   sessions: 'admin/sessions'
 }
+
+  root :to => "public/homes#top"
+  get '/about' => "public/homes#about"
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
